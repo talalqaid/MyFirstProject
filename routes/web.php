947e218c46f297fb15702cf618c1db2ca552af58
talalqaid <talal.qaid69@gmail.com>
2026-07-10 15:54:16 +0100

    new commit

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@ Route::get('/', function () {
 
 
 Route::get('/jobs', function () {
-    return view('jobs',['jobs'=>Job::all()]);
+    $job=Job::with('employer')->get();
+    return view('jobs',['jobs'=>$job]);
 });
 
 Route::get('/jobs/{id}', function($id){
